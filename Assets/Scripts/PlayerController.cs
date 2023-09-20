fa0c29929a528014855bf9a39c9fb7bc74cd4481
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2023-09-19 23:39:01 -0400

    Working on Unity Project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; //Added new namespace.
+
+/**
+*   4 namespaces above are: System.Collections, System.Collections.Generic,
+*   UnityEngine and now UnityEngine.InputSystem.
+**/
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0; //Should be accessible and visible in the inspector module becuase it was set to public.
+
+
+    //private variables will not be accessible from the inspector in Unity or other scripts.
+    private Rigidbody rb; //creates a private variable type Rigidbody called rb.
+    
+
+    //float is used for precision then whole number or integer value.
+    private float movementX;
+    private float movementY;
+
+    /** 
+    *   Is it possible to write it like: private float movementX,movementY;
+    **/
+
+    // Start is called before the first frame update
+    void Start() //when game starts.
+    {
+        rb = GetComponent<Rigidbody>(); //sets value of variable rb by getting refrence to the Rigidbody component attached to the playersphere GameObject.
+        
+    }
+
+    //Created function declaration (aka method?).
+    //movementValue is name of the variable used within the function.
+    //InputValue is the type of variable.
+    //movementValue will store capture and store data of x and y direction.
+    //this type of data will be stored as a vector 2 variable.
+    void OnMove(InputValue movementValue)
+    //Function body (method body/block?)
+    {
+        /** 
+        *   code takes vector2 data from movementValue and stores it in
+        *   vector2 variable created called movementVector.
+        **/
+
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    //FixedUpdate is called just before performing physics calculations(Physics code).
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY); //f signifies it's a float value.
+        
+        rb.AddForce(movement * speed); //were multiplying the force.
+
+    }
+
+}
+
+
+
+
+
+/**
+*  NOTES:
+*  void means function wont return any values.
+*  Variables can store different types of data.(Variables are int, string...)
+*  
+**/
+
+/** 
+*   Functions are start and update.
+*   Update is called once per frame.
+*   Update: Before rendering a frame.
+*   Vector3 Variables stores data accross 3 axis (X,Y,Z so im guessing that mean 3D)
+*   Vector2 I'm guessing is for 2 axis? (X and Y (2D))
+**/
